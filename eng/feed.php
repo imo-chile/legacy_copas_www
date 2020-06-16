4a5b1731b9168c40bb017cc087f6f1b0177f175e
edwin <edwrodrig@gmail.com>
2020-06-16 11:20:45 -0400

    First commit

---
+++ eng/feed.php
@@ -0,0 +1,27 @@
+<?phpxml version="1.0" encoding="ISO-8859-1"?>
+<rss version="2.0" xmlns:atom="http://www.w3.org/2005/Atom">
+    <channel>
+    <atom:link href="http://copas.udec.cl/eng/feed.php" rel="self" type="application/rss+xml" />
+        <title>FONDAP COPAS</title>
+        <description>Center for Oceanographic Research in the eastern South Pacific, University of Concepcion, CHILE</description>
+        <link>http://copas.udec.cl/eng/</link>
+        <lastBuildDate>Tue, 16 Jun 2020 10:33:15 0000</lastBuildDate>
+        <copyright>Copyright 2005-2009 University of Concepcion, Chile</copyright>
+        <category>Research Science East South Pacific Ocean University</category>
+        <item>
+            <title>10 Years</title>
+            <link>http://copas.udec.cl/eng/index.php?prev=84</link>
+            <description>&lt;p&gt;The COPAS Center has fulfilled 10 years of  oceanographic research in the eastern South Pacific using integrated and multidisciplinary perspectives, with the highest standards of international excellence.&lt;/p&gt;
+&lt;p&gt;&lt;a href=&quot;http://copas.udec.cl/eng/news/COPAS_10_Years.pdf&quot; target=&quot;_blank&quot;&gt;Read more&lt;/a&gt;&lt;/p&gt;</description>
+            <pubDate>Sun, 26 Aug 2012 23:00:00 0000</pubDate>
+            <guid>http://copas.udec.cl/eng/index.php?prev=84</guid>
+        </item>
+        <item>
+            <title>Would you like to knowabout our latest research efforts?January 2012</title>
+            <link>http://copas.udec.cl/eng/index.php?prev=81</link>
+            <description>&lt;p&gt;&lt;a href=&quot;http://copas.udec.cl/eng/outreach/newsletters/&quot;&gt;Please read COPAS Newsletter 22&lt;/a&gt; !!!&lt;/p&gt;&lt;p&gt;&lt;a href=&quot;http://copas.udec.cl/eng/outreach/newsletters/&quot;&gt;&lt;img alt=&quot;news&quot; src=&quot;http://copas.udec.cl/eng/news/news_icon.jpg&quot; /&gt;&lt;/a&gt;&lt;/p&gt;</description>
+            <pubDate>Wed, 25 Jan 2012 00:00:00 0000</pubDate>
+            <guid>http://copas.udec.cl/eng/index.php?prev=81</guid>
+        </item>
+    </channel>
+</rss>
---
+++ esp/feed.php
@@ -0,0 +1,26 @@
+<?phpxml version="1.0" encoding="ISO-8859-1"?>
+<rss version="2.0" xmlns:atom="http://www.w3.org/2005/Atom">
+    <channel>
+    <atom:link href="http://copas.udec.cl/esp/feed.php" rel="self" type="application/rss+xml" />
+        <title>FONDAP COPAS</title>
+        <description>Centro de Investigacion Oceanografica en el Pacifico Sur-Oriental, Universidad de Concepcion, CHILE</description>
+        <link>http://copas.udec.cl/esp/</link>
+        <lastBuildDate>Tue, 16 Jun 2020 10:33:15 0000</lastBuildDate>
+        <copyright>Copyright 2005-2009 University of Concepcion, Chile</copyright>
+        <category>Research Science East South Pacific Ocean University</category>
+        <item>
+            <title></title>
+            <link>http://copas.udec.cl/esp/index.php?prev=84</link>
+            <description></description>
+            <pubDate>Mon, 27 Aug 2012 00:00:00 0000</pubDate>
+            <guid>http://copas.udec.cl/esp/index.php?prev=84</guid>
+        </item>
+        <item>
+            <title></title>
+            <link>http://copas.udec.cl/esp/index.php?prev=81</link>
+            <description></description>
+            <pubDate>Wed, 25 Jan 2012 01:00:00 0000</pubDate>
+            <guid>http://copas.udec.cl/esp/index.php?prev=81</guid>
+        </item>
+    </channel>
+</rss>
